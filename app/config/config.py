b1eda269849be79fa555ff00962711b32ee40524
najar1412 <najar1412@users.noreply.github.com>
2017-09-04 18:59:06 -0700

    Delete config.py

--- app/config/config.py
+++
@@ -1,9 +0,0 @@
-BASEURL = 'http://127.0.0.1:5050/gallery/v1'
-
-flask_secret_key = 'SPECIALSECRETKEYWHAAA'
-flask_upload_folder = 'upload'
-flask_allowed_extensions = set(['jpg', 'jpeg'])
-
-aws_access_id = 'AKIAJW2OKKHEQHZ54ASA'
-aws_secret_key = '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'
-aws_bucket = 'gallerystore'
